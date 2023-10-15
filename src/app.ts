c04ce8fc08c45767ee4a099e24f11e78b6dd3fa9
NewmarLucas <newmarlucas@gmail.com>
2023-10-15 10:06:02 -0400

    removendo helmet

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import helmet from 'helmet';
 import { router } from './router';
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(helmet());
 app.use(express.json());
 
 app.use(router);
